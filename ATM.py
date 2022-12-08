279decf921b2f1655a2484dc61f8944a12139b51
JulieAnneFlores4 <118712894+JulieAnneFlores4@users.noreply.github.com>
2022-12-08 04:25:45 +0000

    This method will display if the user wants to do a transactions

--- ATM.py
+++ ATM.py
@@ -85,3 +85,18 @@ account_number = input("Enter your account number: ")
 print("Congratulations! Account created successfully......\n")
  
 atm = ATM(name, account_number)
+
+while True:
+    trans =input("Do you want to do any transaction?(y/n):")
+    if trans == "y":
+        atm.transaction()
+     elif trans == "n":
+print(""" 
+--------------------------------------
+|Thankyou for choosing us as your bank|
+|Visit us again!                      |
+--------------------------------------
+""")
+break
+else:
+    print   ("Wrong command! Enter 'y for YES and  'n' for NO.\n")
